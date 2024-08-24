4f6bfd886d51acd3afcb8cfaff28239c066437fd
Xiaozhu <3158299835>
2024-08-24 23:28:16 +0800

    更新工程文件

--- 01 - Project/00 - template/User/main.c
+++ 01 - Project/00 - template/User/main.c
@@ -1,10 +1,18 @@
 #include "stm32f1xx_hal.h"
+#include "rcc.h"
 
 
+/*函 数 名：
+ *参    数：
+ *返 回 值：
+ *注意事项：
+ */
 int main (void)
 {
-
-
+    HAL_Init();//初始化HAL库
+    RCC_ClockInit();//配置时钟为72MHZ
+    
+    
     while(1)
     {
     
